80f2d7fe4bc26634e4123a90dc0cc9019d80afb5
Ngo Quoc Quy <quy.nqhcm@gmail.com>
2024-03-16 19:18:40 +0700

    Refactor: add query provider (react-query)

--- src/App.js
+++ src/App.js
@@ -4,21 +4,26 @@ import {
   Route,
   Routes,
 } from "react-router-dom";
+import { QueryProvider, QueryClient } from 'react-query';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import SignUp from './page/SignUp/SignUp';
 
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <main>
-        <Routes>
-          <Route path="/" />
-          <Route path="/sign-up" element={<SignUp />}/>
-        </Routes>
-      </main>
-      <Footer/>
+      <QueryProvider client={queryClient}>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" />
+            <Route path="/sign-up" element={<SignUp />}/>
+          </Routes>
+        </main>
+        <Footer/>
+      </QueryProvider> 
     </BrowserRouter>
   );
 }
