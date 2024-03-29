579ef9db19a78f204d769e889185ae064e3ed166
KhanhLam <90090516+klam-12@users.noreply.github.com>
2024-03-29 21:19:45 +0700

    Update: Finish UI view a lecture

--- src/page/lecture/lecture.js
+++ src/page/lecture/lecture.js
@@ -1,9 +1,11 @@
-import {LectureViewContainer} from "./lectureStyle"
+import {HeaderLecture,LectureOptionStyle,CourseContentStyle,CourseContentContainer,LectureOptionContainer} from "./lectureStyle"
 import { useState } from "react";
 
 import { Grid,Box,ListItemButton,ListItemText,ListItemIcon,Divider } from "@mui/material";
 import KeyboardArrowDown from '@mui/icons-material/KeyboardArrowDown';
 import PlayCircleFilledIcon from '@mui/icons-material/PlayCircleFilled';
+import { Link } from 'react-router-dom';
+
 
 
 export default function Lecture(){
@@ -114,6 +116,113 @@ export default function Lecture(){
               },
             ],
         },
+
+        {
+          name: "Introduction to the course 6",
+          lectures: [
+            {
+              title: "Introduction to the course 6",
+              duration: 60,
+              videoID: "SdcdneSdoV4?si=6-5A_cdJLdJ9cohL",
+            },
+            {
+              title: "Introduction to the course 6",
+              duration: 60,
+              videoID: "xNRJwmlRBNU?si=c3ONdCsHXwLHdFEo",
+            },
+            {
+              title: "Introduction to the course 6",
+              duration: 10,
+              videoID: "SdcdneSdoV4?si=6-5A_cdJLdJ9cohL",
+            },
+            {
+              title: "Introduction to the course 6",
+              duration: 10,
+              videoID: "SdcdneSdoV4?si=6-5A_cdJLdJ9cohL",
+            },
+          ],
+        },
+        {
+          name: "Introduction to the course 7",
+      
+          lectures: [
+            {
+              title: "Introduction to the course 7",
+              duration: 10,
+              videoID: "SdcdneSdoV4?si=6-5A_cdJLdJ9cohL",
+            },
+            {
+              title: "Introduction to the course 7",
+              duration: 10,
+              videoID: "SdcdneSdoV4?si=6-5A_cdJLdJ9cohL",
+            },
+            {
+              title: "Introduction to the course 7",
+              duration: 10,
+              videoID: "SdcdneSdoV4?si=6-5A_cdJLdJ9cohL",
+            },
+          ],
+        },
+        {
+          name: "Introduction to the course 8",
+          lectures: [
+            {
+              title: "Introduction to the course 8",
+              duration: 10,
+              videoID: "SdcdneSdoV4?si=6-5A_cdJLdJ9cohL",
+            },
+            {
+              title: "Introduction to the course 8",
+              duration: 10,
+              videoID: "SdcdneSdoV4?si=6-5A_cdJLdJ9cohL",
+            },
+            {
+              title: "Introduction to the course 8",
+              duration: 10,
+              videoID: "SdcdneSdoV4?si=6-5A_cdJLdJ9cohL",
+            },
+          ],
+        },
+        {
+            name: "Introduction to the course 9",
+            lectures: [
+              {
+                title: "Introduction to the course 9",
+                duration: 10,
+                videoID: "SdcdneSdoV4?si=6-5A_cdJLdJ9cohL",
+              },
+              {
+                title: "Introduction to the course 9",
+                duration: 10,
+                videoID: "SdcdneSdoV4?si=6-5A_cdJLdJ9cohL",
+              },
+              {
+                title: "Introduction to the course 9",
+                duration: 10,
+                videoID: "SdcdneSdoV4?si=6-5A_cdJLdJ9cohL",
+              },
+            ],
+        },
+        {
+            name: "Introduction to the course 10",
+            lectures: [
+              {
+                title: "Introduction to the course 10",
+                duration: 10,
+                videoID: "UgIwjLg4ONk?si=KJt1GPzPDU6NHwFX",
+              },
+              {
+                title: "Introduction to the course 10",
+                duration: 10,
+                videoID: "UgIwjLg4ONk?si=KJt1GPzPDU6NHwFX",
+              },
+              {
+                title: "Introduction to the course 10",
+                duration: 10,
+                videoID: "UgIwjLg4ONk?si=KJt1GPzPDU6NHwFX",
+              },
+            ],
+        },
       ];
 
     const [open, setOpen] = useState(false);
@@ -122,7 +231,7 @@ export default function Lecture(){
     const [videoId, setSelectedVideoId] = useState(sections[0].lectures[0].videoID)
     console.log(sections[0].lectures[0].videoID)
 
-    const handleListItemClick = (event, index,sectionName,idVideo) => {
+    const handleListItemClick = (event, index, sectionName,idVideo) => {
         setSelectedIndex(index);
         setSelectedSection(sectionName)
         setSelectedVideoId(idVideo)
@@ -130,6 +239,7 @@ export default function Lecture(){
 
     return (
         <Grid container>
+            <HeaderLecture courseName = "Course Name"></HeaderLecture>
             <Grid item xs={8}>
                 <div>
                     <iframe width="100%" height="500" src={`https://www.youtube.com/embed/${videoId}`}
@@ -137,81 +247,100 @@ export default function Lecture(){
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
                         referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
                 </div>
+
+                <LectureOptionStyle>
+                  <nav>
+                    <ul>
+                      <li><Link to="#">Overview</Link></li>
+                      <li><Link to="#">Reviews</Link></li>
+                    </ul>
+                  </nav>
+                </LectureOptionStyle>
+
+                <LectureOptionContainer></LectureOptionContainer>
+          
             </Grid>
 
             <Grid item xs={4}>
-                {sections.map((section, index) => (
-                    <Box
-                        key={index}
-                        sx={{
-                            bgcolor: open ? 'var(--color-white)' : null,
-                        }}
-                    >
-                        <ListItemButton
-                        alignItems="flex-start"
-                        onClick={() => {setOpen(!open); setSelectedSection(section.name) }}
-                        sx={{
-                            px: 3,
-                            pt: 2,
-                            pb: 2,
-                            '&:hover, &:focus': { '& svg': { opacity: open ? 1 : 0 } },
-                        }}
-                        >
-                        <ListItemText
-                            primary= {section.name}
-                            primaryTypographyProps={{
-                            fontSize: 17,
-                            fontWeight: 'bold',
-                            lineHeight: '20px',
-                            mb: '2px',
-                            }}
-                            secondary= {`${section.lectures.length} lectures`}
-                            secondaryTypographyProps={{
-                            noWrap: true,
-                            fontSize: 12,
-                            lineHeight: '16px',
-                            color: "var(--color-gray-300)",
-                            }}
-                            sx={{ my: 0 }}
-                        />
-                        <KeyboardArrowDown
-                            sx={{
-                            mr: -1,
-                            opacity: 1,
-                            transform: open ? 'rotate(-180deg)' : 'rotate(0)',
-                            transition: '0.2s',
-                            }}
-                        />
-                        </ListItemButton>
+                <CourseContentStyle>
+                  <h3>Course content</h3>
+                </CourseContentStyle>
 
-                        {open && selectedSection == section.name &&
-                        section.lectures.map((item,index) => (
-                            <ListItemButton
-                            key={index}
-                            sx={{ py: 0.5, minHeight: 32,mb:1 }}
-                            selected={selectedIndex === index && selectedSection == section.name}
-                            onClick={(event) => handleListItemClick(event, index,section.name,item.videoID)}
-                            >
-                            <div style={{margin:"10px", color:"var(--color-grey-100)"}}>
-                                <PlayCircleFilledIcon/>
-                            </div>
+                <CourseContentContainer>
+                  {sections.map((section, index) => (
+                      <Box
+                          key={index}
+                          sx={{
+                              bgcolor: open ? 'white' : null,
+                          }}
+                      >
+                          <ListItemButton
+                          alignItems="flex-start"
+                          onClick={() => {setOpen(!open); setSelectedSection(section.name) }}
+                          sx={{
+                              px: 3,
+                              pt: 2,
+                              pb: 2,
+                              '&:hover, &:focus': { '& svg': { opacity: open ? 1 : 0 } },
+                              backgroundColor: "var(--color-gray-100)",
+                          }}
+                          >
+                          <ListItemText
+                              primary= {section.name}
+                              primaryTypographyProps={{
+                              fontSize: 17,
+                              fontWeight: 'bold',
+                              lineHeight: '20px',
+                              mb: '2px',
+                              }}
+                              secondary= {`${section.lectures.length} lectures`}
+                              secondaryTypographyProps={{
+                              noWrap: true,
+                              fontSize: 12,
+                              lineHeight: '16px',
+                              color: "var(--color-gray-300)",
+                              }}
+                              sx={{ my: 0}}
+                          />
+                          <KeyboardArrowDown
+                              sx={{
+                              mr: -1,
+                              opacity: 1,
+                              transform: open ? 'rotate(-180deg)' : 'rotate(0)',
+                              transition: '0.2s',
+                              }}
+                          />
+                          </ListItemButton>
 
-                            <ListItemText
-                                primary={item.title}
-                                primaryTypographyProps={{ fontSize: 14, fontWeight: 'medium', }}
-                                secondary= {`${item.duration} min`}
-                                secondaryTypographyProps={{
-                                noWrap: true,
-                                fontSize: 12,
-                                lineHeight: '16px',
-                                color: "var(--color-gray-300)",
-                                }}
-                            />
-                            </ListItemButton>
-                        ))}
-                        <Divider />
-                    </Box>
-                ))}
+                          {open && selectedSection == section.name &&
+                            section.lectures.map((item,index) => (
+                              <ListItemButton
+                              key={index}
+                              sx={{ py: 0.5, minHeight: 32,mb:1,backgroundColor:"white" }}
+                              selected={selectedIndex === index && selectedSection == section.name}
+                              onClick={(event) => handleListItemClick(event, index,section.name,item.videoID)}
+                              >
+                              <div style={{margin:"10px", color:"var(--color-grey-100)"}}>
+                                  <PlayCircleFilledIcon/>
+                              </div>
+
+                              <ListItemText
+                                  primary={item.title}
+                                  primaryTypographyProps={{ fontSize: 14, fontWeight: 'medium', }}
+                                  secondary= {`${item.duration} min`}
+                                  secondaryTypographyProps={{
+                                  noWrap: true,
+                                  fontSize: 12,
+                                  lineHeight: '16px',
+                                  color: "var(--color-gray-300)",
+                                  }}
+                              />
+                              </ListItemButton>
+                          ))}
+                          <Divider />
+                      </Box>
+                  ))}
+                </CourseContentContainer>
             </Grid>
         </Grid>
     )
--- src/page/lecture/lectureStyle.js
+++ src/page/lecture/lectureStyle.js
@@ -1,8 +1,99 @@
 import styled from "styled-components";
 import { useState } from "react";
 
-import { Grid,Box,ListItemButton,ListItemText,ListItemIcon,Divider } from "@mui/material";
-import KeyboardArrowDown from '@mui/icons-material/KeyboardArrowDown';
-import PlayCircleFilledIcon from '@mui/icons-material/PlayCircleFilled';
+export const HeaderLectureStyle = styled.div`
+  height: 50px;
+  width: 100vw;
+  background-color: var(--color-gray-400);
+  color: white;
+  border-bottom: 0.5px solid white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const HeaderLecture = ({courseName}) => {
+    return(
+        <HeaderLectureStyle>
+            <h3 style={{fontWeight:400}}>{courseName}</h3>
+        </HeaderLectureStyle>
+    )
+}
+
+export const LectureOptionStyle = styled.div`
+    display: flex;
+    width: auto;
+    align-items: center;
+    border-bottom: 1px solid var(--color-gray-200);
+    margin: 0px 20px;
+    margin-bottom: 20px;
+    
+
+    nav ul{
+        list-style: none;
+        display: flex;
+        gap: 20px;
+        padding-left: 10px;
+    }
+
+    nav ul li {
+        width: fit-content;
+    }
+
+    nav ul li a {
+        color: var(--color-gray-300);
+        text-decoration: none;
+        font-weight: 700;
+    }
+
+    nav ul li a:hover {
+        color: var(--color-gray-600);
+    }
+
+`;
+
+export const LectureOptionContainerStyle = styled.div`
+    margin: 20px 40px;
+    
+    .authorName{
+        font-style: italic;
+        color: var(--color-orange-500);
+    }
+`;
+
+export const LectureOptionContainer = () => {
+    return(
+        <LectureOptionContainerStyle>
+            <h2>About this course</h2>
+            <div>
+            Learn Android App Development in both Java &amp; Kotlin Languages. You'll master Android from ZERO to HERO
+            </div>
+
+            <h2>Instructor</h2>
+            <h3 className="authorName">Abbass Masri</h3>
+            <ul>
+                <li>Professional Android app developer with more than 10 years experience.</li>
+                <li>Having a successful apps on playstore with over than +1,000,000 downloads  ( Check them on playstore: )</li>
+                <li>Masters Degree in computer science</li>
+                <li>Teaching in many local schools</li>
+                <li>Having a big youtube channel helping many people to learn android app development and flutter.</li>
+                </ul>
+        </LectureOptionContainerStyle>
+    )
+}
 
 
+export const CourseContentStyle = styled.div`
+    border-bottom: 0.5px solid var(--color-gray-200);
+    h3 {
+        margin-left: 20px;
+    }
+`;
+
+export const CourseContentContainer = styled.div`
+    scroll-snap-type: y mandatory;
+    overflow-y: scroll;
+    height: 100vh;
+    background-color: var(--color-gray-100);
+    position: relative;
+`;
