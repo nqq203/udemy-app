8fdf914beaac961135242dd5f48a2730ded2c655
Ngo Quoc Quy <quy.nqhcm@gmail.com>
2024-04-15 12:57:02 +0700

    UPDATE: update auth context to validate token

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,29 +7,36 @@ const AuthContext = createContext();
 export const useAuth = () => useContext(AuthContext);
 
 const isTokenExpired = (accessToken) => {
+  if (!accessToken || typeof accessToken !== 'string') {
+    // No valid token to decode
+    return true;  // Assuming expired if no valid token
+  }
+
   const decodedToken = jwtDecode(accessToken);
   const currentDate = new Date();
 
   // JWT exp is in seconds
   return decodedToken.exp * 1000 < currentDate.getTime();
-}
+};
 
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [accessToken, setToken]  = useState(localStorage.getItem('accessToken'));
   console.log(!accessToken)
   useEffect(() => {
-    if (!accessToken ) {
-      // Log out the user
-      if (isTokenExpired(accessToken))
-      {
+    if (accessToken) {
+      if (isTokenExpired(accessToken)) {
+        // Token is expired, clear it and update state
         localStorage.removeItem('accessToken');
-        setToken(localStorage.getItem('accessToken'));
-      setIsAuthenticated(false);
+        setToken(null);
+        setIsAuthenticated(false);
+      } else {
+        // Token is valid
+        setIsAuthenticated(true);
       }
-    }
-    else {
-      setIsAuthenticated(true);
+    } else {
+      // No token, ensure user is logged out
+      setIsAuthenticated(false);
     }
   }, [accessToken]);
 
